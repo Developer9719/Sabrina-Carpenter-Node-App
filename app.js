1dc542f74bdff31a9fccc40abfef03670f581efe
Justin Battrick <justin.bat19@hotmail.com>
2024-11-08 00:12:31 -0500

    Set up all pages needed for the site. Set up URL paths and router locations

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Set up routers locations
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var musicRouter = require('./routes/music');
+var videoRouter = require('./routes/videos');
+var tourRouter = require('./routes/tour');
+var shopRouter = require('./routes/shop');
+var signupRouter = require('./routes/signup');
 
 var app = express();
 
@@ -19,8 +26,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setting up URL paths
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/music', musicRouter);
+app.use('/videos', videoRouter);
+app.use('/tour', tourRouter);
+app.use('/shop', shopRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
